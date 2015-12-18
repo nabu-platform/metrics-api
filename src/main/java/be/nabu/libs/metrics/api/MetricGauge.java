bb4413bda338c4f2edb6f079794f5d0ec50640e6
alex <alex@nabu.be>
2015-12-18 07:10:22 +0100

    Added gauges

---
+++ src/main/java/be/nabu/libs/metrics/api/MetricGauge.java
@@ -0,0 +1,5 @@
+package be.nabu.libs.metrics.api;
+
+public interface MetricGauge {
+	public long getValue();
+}
--- src/main/java/be/nabu/libs/metrics/api/MetricInstance.java
+++ src/main/java/be/nabu/libs/metrics/api/MetricInstance.java
@@ -1,6 +1,12 @@
 package be.nabu.libs.metrics.api;
 
+import java.util.concurrent.TimeUnit;
+
 public interface MetricInstance {
+	/**
+	 * Based on internal time capture, add a duration value
+	 */
+	public void duration(String id, long duration, TimeUnit timeUnit);
 	/**
 	 * Start a new timer
 	 */
@@ -17,4 +23,8 @@ public interface MetricInstance {
 	 * Set a certain value that is not part of a series, it is simply a single state
 	 */
 	public void set(String id, long value);
+	/**
+	 * Sets a certain value with a gauge that can return the actual value 
+	 */
+	public void set(String id, MetricGauge gauge);
 }
