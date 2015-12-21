798f9ee761cf23aa1a8ae8adf2313533f10dd542
alex <alex@nabu.be>
2015-12-21 08:49:01 +0100

    Added timeunit to the timer

--- src/main/java/be/nabu/libs/metrics/api/MetricTimer.java
+++ src/main/java/be/nabu/libs/metrics/api/MetricTimer.java
@@ -1,6 +1,9 @@
 package be.nabu.libs.metrics.api;
 
+import java.util.concurrent.TimeUnit;
+
 public interface MetricTimer {
 	public long stop();
 	public MetricInstance getMetrics();
+	public TimeUnit getTimeUnit();
 }
